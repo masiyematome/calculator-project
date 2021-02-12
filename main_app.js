34212e5e6d0cf441fefc9324653cf9d388066b04
masiyematome <matomemasiye7@gmail.com>
2021-02-12 19:33:00 +0200

    Code that replaces the number on the screen if the number is a zero

--- main_app.js
+++ main_app.js
@@ -1,27 +1,31 @@
 
 
-const numbersOnScreen = document.querySelector(".numbers-on-screen-input");
+const calculatorScreen = document.querySelector(".calculator-screen");
 const calculatorKeys = document.querySelector(".calculator-keys");
 
-calculatorKeys.addEventListener("click" , event => {
-    if(event.target.matches("button")){
-        
+calculatorKeys.addEventListener("click", event => {
+
+    if (event.target.matches("button")) {
         const key = event.target;
         const keyAction = key.dataset.action;
+        const keyContent = key.textContent;
+        const displayedNumber = calculatorScreen.textContent;
 
-        if(!(keyAction)){
-            console.log("Number key");
+        if (!(keyAction)) {
+            if(displayedNumber == 0){
+                calculatorScreen.textContent = keyContent;
+            }
         }
 
-        else if(keyAction == "add" || keyAction == "divide" || keyAction == "multiply" || keyAction == "subtract"){
-            console.log("Operator Key");
+        else if (keyAction == "add" || keyAction == "divide" || keyAction == "multiply" || keyAction == "subtract") {
+            console.log("Operator key");
         }
 
-        else if(keyAction == "decimal"){
+        else if (keyAction == "decimal") {
             console.log("Decimal key");
         }
 
-        else if(keyAction == "clear"){
+        else if (keyAction == "clear") {
             console.log("Clear key");
         }
 
@@ -29,4 +33,5 @@ calculatorKeys.addEventListener("click" , event => {
             console.log("Equal key");
         }
     }
+
 })

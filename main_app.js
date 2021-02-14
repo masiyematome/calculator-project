e6b0569d73f4b1120820e042a8b3aa2117f02a35
masiyematome <matomemasiye7@gmail.com>
2021-02-14 18:40:42 +0200

    Code that highlights the operator key pressed(active)

--- main_app.js
+++ main_app.js
@@ -1,39 +1,43 @@
-
-
 const calculatorScreen = document.querySelector(".calculator-screen");
 const calculatorKeys = document.querySelector(".calculator-keys");
 
-calculatorKeys.addEventListener("click", event => {
+calculatorKeys.addEventListener("click", (event) => {
     if(event.target.matches("button")){
+        
         const key = event.target;
         const keyAction = key.dataset.action;
-        const keyContent = key.textContent;
-        const displayedNumber = calculatorScreen.textContent;
+        const keyContent = key.innerText;
+
+        Array.from(key.parentElement.children).forEach(individualKey => {
+            individualKey.classList.remove("is-depressed");
+        })
 
         if(!keyAction){
-            if(displayedNumber == 0){
-                calculatorScreen.textContent = keyContent;
+            if(calculatorScreen.innerText == 0){
+                calculatorScreen.innerText = keyContent;
             }
 
             else{
-                calculatorScreen.textContent = displayedNumber + keyContent;
+                calculatorScreen.innerText = calculatorScreen.innerText + keyContent;
             }
         }
 
-        else if(keyAction == "add" || keyAction == "multiply" || keyAction == "subtract" || keyAction == "divide"){
-            console.log("Operator key");
+        else if(keyAction == "add" || keyAction == "subtract" || keyAction == "divide" || keyAction == "multiply"){
+            key.classList.add("is-depressed");
+            
         }
 
         else if(keyAction == "decimal"){
-            calculatorScreen.textContent = displayedNumber + ".";
+            calculatorScreen.innerText = calculatorScreen.innerText + ".";
         }
 
-        else if(keyAction == "clear"){
+        else if(keyAction == "calculate"){
+            console.log("Equal sign");
+        }
+
+        else {
             console.log("Clear key");
         }
 
-        else{
-            console.log("Equal key");
-        }
-    }
+;    }
 })

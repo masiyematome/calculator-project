7b92aff735260421deed88b7da4064c8ee8a656d
masiyematome <matomemasiye7@gmail.com>
2021-04-06 17:12:01 +0200

    New code for updating the screen display

--- main_app.js
+++ main_app.js
@@ -1,30 +1,14 @@
 
 const calculatorScreen = document.querySelector(".calculator-screen");
 const calculatorKeys = document.querySelector(".calculator-keys");
-const operatorKeys = document.querySelectorAll(".operator");
 
+const calculator = {
+    valueOnScreen : '12',
+    firstOperand : null,
+    waitingForSecondOperand : false,
+    operator : null,
+}
 
-calculatorKeys.addEventListener("click" , (event)=> {
-    const clickedKey = event.target;
-
-    if(clickedKey.classList.contains("operator")){
-        console.log("I am an operator key");
-    }
-
-    else if(clickedKey.classList.contains("equal-key")){
-        console.log("I am an equal key");
-    }
-
-    else if(clickedKey.classList.contains("clear")){
-        console.log("I am a clear key");
-    }
-
-    else if(clickedKey.classList.contains("decimal")){
-        console.log("I am a decimal key");
-    }
-
-    else{
-        console.log("I am a number key");
-    }
-
-})
+~function updateDisplay(){
+    calculatorScreen.textContent = calculator.valueOnScreen;
+}();

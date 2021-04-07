591c252c2beec937f36b1eb071d76b20c1c3e6c9
masiyematome <matomemasiye7@gmail.com>
2021-04-07 22:08:17 +0200

    Got the code to be able to display numbers on the screen

--- main_app.js
+++ main_app.js
@@ -1,14 +1,63 @@
-
-const calculatorScreen = document.querySelector(".calculator-screen");
-const calculatorKeys = document.querySelector(".calculator-keys");
+const calculatorkeys = document.querySelector(".calculator-keys");
 
 const calculator = {
-    valueOnScreen : '12',
-    firstOperand : null,
-    waitingForSecondOperand : false,
-    operator : null,
+    valueOnScreen: '0',
+    firstOperand: null,
+    waitingForSecondOperand: false,
+    operator: null,
 }
 
-~function updateDisplay(){
-    calculatorScreen.textContent = calculator.valueOnScreen;
-}();
+function updateDisplay() {
+    const calculatorScreen = document.querySelector(".calculator-screen");
+    calculatorScreen.value = calculator.valueOnScreen;
+}
+
+updateDisplay();
+
+function appendDigit(digit) {
+    if (calculator.valueOnScreen == '0') {
+        calculator.valueOnScreen = digit;
+    }
+
+    else {
+        calculator.valueOnScreen = calculator.valueOnScreen + digit;
+    }
+}
+
+
+calculatorkeys.addEventListener("click", (event) => {
+    const { target } = event;
+
+    if (!(target.matches("button"))) {
+        return;
+    }
+
+    else {
+        if (target.classList.contains("operator")) {
+            console.log('I am an operator', target.value);
+        }
+
+        else if (target.classList.contains("decimal")) {
+            console.log("I am a decimal key", target.value);
+        }
+
+        else if (target.classList.contains("equal-sign")) {
+            console.log("I am an equal key", target.value);
+        }
+
+        else if (target.classList.contains("clear")) {
+            console.log("I will clear this thing broer", target.value);
+        }
+
+        else {
+            console.log("I am a decimal key");
+        }
+
+        appendDigit(target.value);
+        updateDisplay();
+        
+    }
+})
+
+/**Playing around with code**/
+

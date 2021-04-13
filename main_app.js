4cab3f2e3a91b6e849ea89cf367f624bbbab4dd6
masiyematome <matomemasiye7@gmail.com>
2021-04-13 19:01:05 +0200

    Code that reads digits and decimal point with keyboard presses

--- main_app.js
+++ main_app.js
@@ -35,13 +35,13 @@ function appendDigit(digit) {
 function appendDot(dot) {
     const { valueOnDisplay } = calculator;
 
-    if(calculator.waitingForSecondOperand == true){
+    if (calculator.waitingForSecondOperand == true) {
         calculator.valueOnDisplay = '0.';
         calculator.waitingForSecondOperand = false;
         return;
     }
 
-    else{
+    else {
         if (valueOnDisplay.includes(dot)) {
             return;
         }
@@ -53,10 +53,10 @@ function appendDot(dot) {
 }
 
 function handleOperators(clickedOperator) {
-    const { firstOperand, valueOnDisplay ,operator } = calculator;
+    const { firstOperand, valueOnDisplay, operator } = calculator;
     const storedValue = parseFloat(valueOnDisplay);
 
-    if(operator && calculator.waitingForSecondOperand == true){
+    if (operator && calculator.waitingForSecondOperand == true) {
         calculator.operator = clickedOperator;
         return;
     }
@@ -65,8 +65,8 @@ function handleOperators(clickedOperator) {
         calculator.firstOperand = storedValue;
     }
 
-    else if(operator){
-        const result = calculate(firstOperand , storedValue , operator);
+    else if (operator) {
+        const result = calculate(firstOperand, storedValue, operator);
         calculator.firstOperand = String(result);
         calculator.valueOnDisplay = result;
     }
@@ -96,7 +96,7 @@ function calculate(firstOperand, secondOperand, operator) {
     }
 }
 
-function clearCalculator(){
+function clearCalculator() {
     calculator.valueOnDisplay = '0';
     calculator.firstOperand = null;
     calculator.waitingForSecondOperand = false;
@@ -137,3 +137,72 @@ function keys(event) {
     }
 }
 
+document.addEventListener("keypress", keyPresses);
+
+function keyPresses(event) {
+
+    const { target } = event;
+
+    if (target.code === 97) {
+        console.log("You pressed 1");
+    }
+
+}
+
+
+
+document.onkeyup = function (event) {
+    const clickedKey = event;
+    const clickedKeyCode = clickedKey.keyCode;
+
+    switch (clickedKeyCode) {
+
+        case 96:
+            appendDigit('0');
+            break;
+
+        case 97:
+            appendDigit('1')
+            break;
+
+        case 98:
+            appendDigit('2')
+            break;
+
+        case 99:
+            appendDigit('3');
+            break;
+
+        case 100:
+            appendDigit('4');
+            break;
+
+        case 101:
+            appendDigit('5');
+            break;
+
+        case 102:
+            appendDigit('6');
+            break;
+
+        case 103:
+            appendDigit('7');
+            break;
+
+        case 104:
+            appendDigit('8');
+            break;
+
+        case 105:
+            appendDigit('9');
+            break;
+
+        case 110:
+            appendDot('.');
+            break;
+
+    }
+
+    updateScreenDisplay();
+
+}
